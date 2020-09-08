fee1b8d6f5b8f3395bbadf89b56eb6f82023f374
rahmjan <rahmjan@gmail.com>
2020-09-08 12:36:43 +0200

    User registration

--- src/main/java/com.memorynotfound.spring.security/web/Run.java
+++ src/main/java/com.memorynotfound.spring.security/web/Run.java
@@ -1,13 +1,12 @@
-package frontend;
+package com.memorynotfound.spring.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServingWebContentApplication {
+public class Run {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServingWebContentApplication.class, args);
+        SpringApplication.run(Run.class, args);
     }
-
 }
